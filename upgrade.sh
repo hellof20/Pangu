78dccbcc4fe1ed2b0e29c86debfccdb8cf845b85
pwm <pwm@google.com>
2022-08-18 17:50:19 +0800

    20220818 17:50 update

--- upgrade.sh
+++ upgrade.sh
@@ -2,7 +2,7 @@
 
 cd /tmp/$DEPLOY_ID/$solution_id/$tf_path
 git pull > git.log 2>&1
-terraform apply -auto-approve -var-file="terraform.tfvars" -var="access_token=$access_token" -no-color >> /tmp/$DEPLOY_ID/$solution_id/deploy.log 2>&1
+terraform apply -auto-approve -var-file="terraform.tfvars" -var="access_token=$access_token" -no-color > /tmp/$DEPLOY_ID/$solution_id/deploy.log 2>&1
 if [ $? -eq 0 ]; then
     mysql --host=$host --user=$user --password=$password --database="ads" --execute="update deploy set status='success' where id='$DEPLOY_ID';"
 else
