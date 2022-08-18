73695d11f990cce2a66cc5a7fd3b6ed859dd8248
pwm <pwm@google.com>
2022-08-18 17:51:29 +0800

    20220818 17:50 update

--- sql/ads.sql
+++ sql/ads.sql
@@ -76,8 +76,7 @@ DROP TABLE IF EXISTS `permission`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `permission` (
-  `scope` varchar(100) NOT NULL,
-  `solution_id` varchar(100) NOT NULL
+  `scope` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
