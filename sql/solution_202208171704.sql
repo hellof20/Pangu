9e85c277067428a09ce453be4112102922240e9b
Pan, Wen-Ming <smalljelly2018@gmail.com>
2022-08-19 13:59:37 +0800

    Add files via upload

---
+++ sql/solution_202208171704.sql
@@ -0,0 +1,4 @@
+INSERT INTO ads.solution (id,name,url) VALUES
+	 ('baremetal','Bare Metal','https://professional-services.googlesource.com/solutions/ads-bare-metal'),
+	 ('lego','LEGO','https://professional-services.googlesource.com/solutions/ads-bi-suite'),
+	 ('vm-test','VM Test','https://github.com/hellof20/tf-tutorial.git');
