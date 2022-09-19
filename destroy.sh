c61ad939fe8579e40dae2f834aa106d8299c7b92
pwm <pwm@google.com>
2022-09-19 12:14:12 +0800

    bugfix: change terraform.tfvars to pangu.tfvars

--- destroy.sh
+++ destroy.sh
@@ -3,7 +3,7 @@
 deploy_path=/data/pangu
 cd $deploy_path/$DEPLOY_ID/$solution_id/$tf_path
 
-terraform apply -destroy -auto-approve -var-file="terraform.tfvars" -var="access_token=$access_token" -no-color > $deploy_path/$DEPLOY_ID/deploy.log 2>&1
+terraform apply -destroy -auto-approve -var-file="pangu.tfvars" -var="access_token=$access_token" -no-color > $deploy_path/$DEPLOY_ID/deploy.log 2>&1
 
 if [ $? -eq 0 ]; then
     mysql --host=$host --user=$user --password=$password --database="ads" --execute="update deploy set status='empty' where id='$DEPLOY_ID';"
